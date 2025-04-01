d5bb24d144abc33f7fd7547c76d818819c2f8ed9
TRMrDucky <vv094673@gmail.com>
2025-04-01 12:04:46 -0700

    Update ClienteRegistradoDTO.java

--- RollAndCode/DominioRollAndCode/src/main/java/com/mycompany/dominiorollandcode/dtos/ClienteRegistradoDTO.java
+++ RollAndCode/DominioRollAndCode/src/main/java/com/mycompany/dominiorollandcode/dtos/ClienteRegistradoDTO.java
@@ -4,10 +4,38 @@
  */
 package com.mycompany.dominiorollandcode.dtos;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+
 /**
  *
  * @author 52644
  */
 public class ClienteRegistradoDTO {
+    Long id;
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String email;
+    private int telefono;
+    private Calendar fechaRegistro;
+    private int cantidadVisitas;
+    private BigDecimal gastoTotal;
+    private int puntosFidelidad;    
+
+    public ClienteRegistradoDTO(Long id, String nombres, String apellidoPaterno, String apellidoMaterno, String email, int telefono, Calendar fechaRegistro, int cantidadVisitas, BigDecimal gastoTotal, int puntosFidelidad) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.email = email;
+        this.telefono = telefono;
+        this.fechaRegistro = fechaRegistro;
+        this.cantidadVisitas = cantidadVisitas;
+        this.gastoTotal = gastoTotal;
+        this.puntosFidelidad = puntosFidelidad;
+    }
+    
+    public ClienteRegistradoDTO(){}
     
 }
