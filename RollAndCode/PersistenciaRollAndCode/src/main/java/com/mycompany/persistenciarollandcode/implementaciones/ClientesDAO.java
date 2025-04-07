33545c722e306bac035ad36ecb56db92cee0b275
TRMrDucky <vv094673@gmail.com>
2025-04-06 22:01:19 -0700

    agregada lógica para la búsqueda

--- RollAndCode/PersistenciaRollAndCode/src/main/java/com/mycompany/persistenciarollandcode/implementaciones/ClientesDAO.java
+++ RollAndCode/PersistenciaRollAndCode/src/main/java/com/mycompany/persistenciarollandcode/implementaciones/ClientesDAO.java
@@ -1,14 +1,17 @@
 package com.mycompany.persistenciarollandcode.implementaciones;
 
+import com.mycompany.dominiorollandcode.dtos.ClienteDTO;
 import com.mycompany.dominiorollandcode.dtos.RegistrarClienteDTO;
 import com.mycompany.dominiorollandcode.entidades.ClienteFrecuente;
 import com.mycompany.persistenciarollandcode.IClientesDAO;
 import com.mycompany.persistenciarollandcode.conexion.ManejadorConexiones;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -83,5 +86,67 @@ public class ClientesDAO implements IClientesDAO {
                 "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
         Matcher matcher = patronEmail.matcher(correo);
         return matcher.matches();
+    }
+
+    @Override
+    public List<ClienteDTO> buscarClientes() {
+        EntityManager em = ManejadorConexiones.getEntityManager();
+        String jpql = """
+                      SELECT NEW com.mycompany.dominiorollandcode.dtos.ClienteDTO(
+                      c.id,
+                      c.nombres,
+                      c.apellidoPaterno,
+                      c.correoElectronico,
+                      c.telefono,
+                      c.gastoTotal
+                      )
+                      FROM ClienteFrecuente c
+                      """;
+        TypedQuery<ClienteDTO> query = em.createQuery(jpql, ClienteDTO.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<ClienteDTO> buscarClientesNombre(String nombre) {
+        EntityManager em = ManejadorConexiones.getEntityManager();
+        String jpql = """
+                      SELECT NEW com.mycompany.dominiorollandcode.dtos.ClienteDTO(
+                      c.id,
+                      c.nombres,
+                      c.apellidoPaterno,
+                      c.correoElectronico,
+                      c.telefono,
+                      c.gastoTotal
+                      )
+                      FROM ClienteFrecuente c 
+                      WHERE c.nombres LIKE :nombres
+                      """;
+        TypedQuery<ClienteDTO> query = em.createQuery(jpql, ClienteDTO.class);
+        query.setParameter("nombres", "%"+nombre+"%");
+        return query.getResultList();
+    }
+
+    @Override
+    public List<ClienteDTO> buscarClientesCorreo(String correo) {
+        EntityManager em = ManejadorConexiones.getEntityManager();
+        String jpql = """
+                      SELECT NEW com.mycompany.dominiorollandcode.dtos.ClienteDTO(
+                      c.id,
+                      c.nombres,
+                      c.apellidoPaterno,
+                      c,correoElectronico,
+                      c.telefono,
+                      c.gastoTotal
+                      )
+                      FROM ClienteFrecuente c
+                      """;
+        TypedQuery<ClienteDTO> query = em.createQuery(jpql, ClienteDTO.class);
+        return query.getResultList();
+
+    }
+
+    @Override
+    public List<ClienteDTO> buscarClientesTelefono(String telefono) {
+
     }
 }
